fbe16b335e4542ab3c2cba9da32e5ab3e70c4ce5
Suresh Khadka <khadkasuresh647@gmail.com>
2026-02-25 09:49:32 +0545

    first commit

---
+++ grocery/models.py
@@ -0,0 +1,14 @@
+# Create your models here.
+from django.db import models
+
+
+class GroceryItem(models.Model):
+    name = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-created_at']
---
+++ grocery/views.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import render
+from .models import GroceryItem
+
+
+def index(request):
+    """Display all grocery items"""
+    items = GroceryItem.objects.all()
+    context = {
+        'items': items,
+    }
+    return render(request, 'grocery/index.html', context)
